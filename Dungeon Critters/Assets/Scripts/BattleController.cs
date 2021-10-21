764e0c9510699db5eae06a02f74f2efdf996299d
Andrei Savu <savu.andrei96@outlook.com>
2021-10-22 02:00:09 +0300

    Fight mechanic
    Very basic fight mechanic introduced

---
+++ Dungeon Critters/Assets/Scripts/BattleController.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleController : MonoBehaviour
+{
+    public List<CritterStats> critters = new List<CritterStats>();
+    public List<CritterStats> t1 = new List<CritterStats>();
+    public List<CritterStats> t2 = new List<CritterStats>();
+
+    void Start()
+    {
+        foreach (CritterStats critter in critters)
+        {
+            if (critter.enemy)
+            {
+                t2.Add(critter);
+            }
+            else
+            {
+                t1.Add(critter);
+            }
+
+            critter.onDeath.AddListener(RemoveCritter);
+        }
+        print(t1.Count);
+        print(t2.Count);
+        StartCoroutine(FightRoutine());
+    }
+
+    IEnumerator FightRoutine()
+    {
+        SortBySpeed();
+        foreach(CritterStats critter in critters)
+        {
+            if (critter.dead) { continue; }
+            if (!critter.enemy)
+            {
+                if (t2.Count == 0) { break; }
+                CritterStats target = SelectTarget(t2);
+                critter.Attack(target);
+            }
+            else
+            {
+                if (t1.Count == 0) { break; }
+                CritterStats target = SelectTarget(t1);
+                critter.Attack(target);
+            }
+
+            yield return new WaitForSeconds(0.3f);
+        }
+        if (t1.Count > 0 && t2.Count > 0)
+        {
+            StartCoroutine(FightRoutine());
+        }
+    }
+
+    void SortBySpeed()
+    {
+        critters.Sort((x, y) => y.speed - x.speed);
+    }
+
+    CritterStats SelectTarget(List<CritterStats> possibleTargets)
+    {
+        for (int i = 0; i < 20; i++)
+        {
+            int i1 = Random.Range(0, possibleTargets.Count);
+            int i2 = Random.Range(0, possibleTargets.Count);
+
+            if (i1 == i2) { continue; }
+
+            CritterStats temp = possibleTargets[i1];
+            possibleTargets[i1] = possibleTargets[i2];
+            possibleTargets[i2] = temp;
+        }
+
+        return possibleTargets[0];
+    }
+
+
+    void RemoveCritter(CritterStats critter)
+    {
+        if (critter.enemy)
+        {
+            t2.Remove(critter);
+        }
+        else
+        {
+            t1.Remove(critter);
+        }
+    }
+}
---
+++ Dungeon Critters/Assets/Scripts/CritterStats.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class CritterEvent : UnityEvent<CritterStats>
+{
+}
+
+
+public class CritterStats : MonoBehaviour
+{
+    public int health;
+    public int maxHealth;
+    public int attack;
+    public int speed;
+    public bool enemy;
+
+    public bool dead;
+
+    public Slider healthSlider;
+
+    public CritterEvent onDeath = new CritterEvent();
+
+    private void Awake()
+    {
+        maxHealth = Random.Range(100, 200);
+        health = maxHealth;
+        attack = Random.Range(5, 10);
+        speed = Random.Range(10, 100);
+    }
+
+    public void Attack(CritterStats target)
+    {
+        StartCoroutine(AttackRoutine(target));
+    }
+
+    IEnumerator AttackRoutine(CritterStats target)
+    {
+        transform.GetChild(0).GetComponent<SpriteRenderer>().color = Color.green;
+        target.transform.GetChild(0).GetComponent<SpriteRenderer>().color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        transform.GetChild(0).GetComponent<SpriteRenderer>().color = Color.white;
+        target.transform.GetChild(0).GetComponent<SpriteRenderer>().color = Color.white;
+        target.TakeDamage(attack);
+    }
+
+    public void TakeDamage(int amount)
+    {
+        health -= Mathf.Max(0, amount);
+        UpdateHealthSlider();
+        if (health <= 0) {
+            onDeath.Invoke(this);
+            dead = true;
+            transform.GetChild(0).GetComponent<SpriteRenderer>().color = Color.gray;
+        }
+    }
+
+    void UpdateHealthSlider()
+    {
+        float percentage = health / (float)maxHealth;
+        healthSlider.value = percentage;
+    }
+}
